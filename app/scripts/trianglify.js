fdb45ec5bf0c40db743f1fe6cbd98f64aa43484e
jamall harris <jharri34@uab.edu>
2015-02-07 11:15:02 -0600

    getting close just finished adding most of the basic elements

---
+++ app/scripts/trianglify.js
@@ -0,0 +1,3 @@
+var t = new Trianglify();
+var pattern = t.generate(document.body.clientWidth, document.body.clientHeight);
+document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
